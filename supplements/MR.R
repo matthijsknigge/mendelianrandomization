929fe1a5ebd2bde9eed51e45a24ca09ed56f3ae2
matthijsknigge <matthijsknigge@hotmail.com>
2017-11-13 11:44:38 +0100

    test file

--- supplements/MR.R
+++ supplements/MR.R
@@ -4,21 +4,21 @@ library(data.table)
 
 # outcome
 # o <- fread("~/Desktop/eo_baso_sum_build37_171771_20161212.txt", header = T)
-o <- fread("~/Bioinformatics/Src/R/cluster_results/GLGC_exome/data/GLGC_exome_TC_release.txt", header = T)
+e <- fread("~/Bioinformatics/Src/R/cluster_results/GLGC_exome/data/GLGC_exome_TC_release.txt", header = T)
 
 # exposure
-e <- fread("~/Bioinformatics/Src/R/cluster_results/outcome_without_HLL_region/outcome/celiac_removed/celiac_2011.txt", header = T, sep = " ")
+o <- fread("~/Bioinformatics/Src/R/cluster_results/outcome_without_HLL_region/outcome/celiac_removed/celiac_2011.txt", header = T, sep = " ")
 
 # calc se
 e$se <- mr.calculate.se(B = e$Z_OR, pval = e$P)
 o$se <- mr.calculate.se(B = o$Z_OR, pval = o$P)
 
 # pre process
-e <- mr.pre.process(B = e$Z_OR, B.se = e$se, pval = e$P, effect_allele = e$effect_allele, other_allele = e$other_allele, SNP = e$SNP)
+e <- mr.pre.process(B = e$Beta, B.se = e$SE, pval = e$`P-value`, effect_allele = e$A1, other_allele = e$A2, SNP = e$rsid)
 
 # harmonize
-h <- mr.harmonize(By = o$Beta, Bx = e$beta, By.se = o$SE, Bx.se = e$se, outcome.pval = o$`P-value`, exposure.pval = e$pval, outcome.effect_allele = o$A1, exposure.effect_allele = e$effect_allele, exposure.other_allele = e$other_allele,
-                  outcome.SNP = o$rsid, exposure.SNP = e$SNP)
+h <- mr.harmonize(By = o$Z_OR, Bx = e$beta, By.se = o$se, Bx.se = e$se, outcome.pval = o$P, exposure.pval = e$pval, outcome.effect_allele = o$effect_allele, exposure.effect_allele = e$effect_allele, exposure.other_allele = e$other_allele,
+                  outcome.SNP = o$SNP, exposure.SNP = e$SNP)
 
 # clump
 h <- mr.clump(data = h, refdat = "~/Bioinformatics/1000G/EUR_1000G_phase3_vcf", verbose = T)
@@ -49,9 +49,9 @@ ivw.Q <- mr.inverse.variance.weighted.method(By = h[which(h$cochran.Q == 1), ]$B
 
 # plot
 
-p <- mr.plot(By = h$By, Bx = h$Bx, By.se = h$By.se, Bx.se = h$Bx.se, iv = h$iv, iv.se = h$iv.se, ivw = ivw$ivw, egger = egger$egger, egger.i = egger$egger.i, ivw.Q = ivw.Q$ivw,
+p <- mr.plot(By = h$By, Bx = h$Bx, By.se = h$By.se, Bx.se = h$Bx.se, iv = h$iv, iv.se = h$iv.se, ivw = ivw$ivw, egger = egger$egger, egger.i = egger$egger.i, ivw.Q = ivw.Q$ivw, chochran.Q = h$cochran.Q,
              egger.Q = egger.Q$egger, egger.i.Q = egger.Q$egger.i, egger.p.fdr = egger$egger.p, ivw.p.fdr = ivw$ivw.p, egger.i.p = egger$egger.i.p, exposure.name =  "Celiac 2011",
-             outcome.name = "eo_baso_sum_build37_171771_20161212")
+             outcome.name = "GLGC_exome_TC_release")
 ggdraw(p)
 
 ggsave(filename = "~/Desktop/test.png", plot = ggdraw(p))
@@ -59,6 +59,17 @@ ggsave(filename = "~/Desktop/test.png", plot = ggdraw(p))
 
 ivw
 
+ivw.fdr[length(ivw.fdr-1)]
+
+ivw.fdr <- p.adjust(p = c(m.2011$beta.ivw.p, ivw$ivw.p), method = "fdr", n = length(m.2011$beta.ivw.p)+1)
+egger.fdr <- p.adjust(p = c(m.2011$beta.egger.p, egger$egger.p), method = "fdr", n = length(m.2011$beta.egger.p)+1)
+
+
+m.2011 <- read.table("~/Bioinformatics/Src/R/cluster_results/outcome_without_HLL_region/final_tables/master.table.methods.2011.txt", header = T)
+m.2010 <- read.table("~/Bioinformatics/Src/R/cluster_results/outcome_without_HLL_region/final_tables/master.table.methods.2010.txt", header = T)
+
+m.2011 <- m.2011[which(m.2011$beta.ivw.p.fdr < .05 | m.2011$beta.egger.p.fdr < .05), ]
+m.2010 <- m.2010[which(m.2010$beta.ivw.p.fdr < .05 | m.2010$beta.egger.p.fdr < .05), ]
 
 
 
