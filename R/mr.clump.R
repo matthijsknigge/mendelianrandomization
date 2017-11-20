5786747fc9da29c2ac145f8bd88e21fed16026f7
matthijsknigge <matthijsknigge@hotmail.com>
2017-11-20 20:09:09 +0100

    Check size of input data

--- R/mr.clump.R
+++ R/mr.clump.R
@@ -18,6 +18,10 @@
 #' @return data.frame with removed snps
 mr.clump <- function(data, refdat, clump_kb = 1000, clump_r2 = .1, clump_p1 = 5*10^-8, clump_p2 = 5*10^-8, verbose = FALSE, SNPs.of.opposite.file){
   require(R.utils)
+  # check size
+  if(length(data$SNP) == 1){
+    return(data)
+  }
   # check if tempt exists
   if(!dir.exists(paste0(system.file(package="mendelianRandomization", "executables"), "/temp_clump"))){
     dir.create(file.path(system.file(package="mendelianRandomization", "executables"), "temp_clump"))
