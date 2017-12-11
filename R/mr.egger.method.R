caf295ca777513ec06be3918e66a0a28faf08ba3
matthijsknigge <matthijsknigge@hotmail.com>
2017-12-11 11:58:33 +0100

    Bug in subset option fixed

--- R/mr.egger.method.R
+++ R/mr.egger.method.R
@@ -22,8 +22,8 @@
 #'         egger.i.p: p-value of intercept
 mr.egger.method <- function(By, Bx, By.se, Bx.se, subset = NULL){
   # if sum subset == 0, do not apply
-  if(sum(subset) == 0){
-    subset <- NULL
+  if(!is.null(subset) & sum(subset) == 0){
+    return(list(egger = NA, egger.se = NA, egger.p = NA, egger.i = NA, egger.i.se = NA, egger.i.p = NA))
   }
   # if subset is used, filter data
   if(!is.null(subset)){
@@ -31,10 +31,6 @@ mr.egger.method <- function(By, Bx, By.se, Bx.se, subset = NULL){
     Bx <- Bx[which(subset == 1)]
     By.se <- By.se[which(subset == 1)]
     Bx.se <- Bx.se[which(subset == 1)]
-  }
-  # test if vector is empty
-  if(length(By) == 0){
-    return(list(egger = NA, egger.se = NA, egger.p = NA, egger.i = NA, egger.i.se = NA, egger.i.p = NA))
   }
   # if not enough data
   if(length(By) < 3){
